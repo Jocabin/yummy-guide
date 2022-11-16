9ccbfbc030ebdda216e17e1281847adf0ac45547
Jocabin <kobyfar@hotmail.com>
2022-11-16 22:32:56 +0100

    fix deploy

--- src/routes/etablissements/[name]/+page.svelte
+++ src/routes/etablissements/[name]/+page.svelte
@@ -1,18 +1,6 @@
 <script>
-    import {supabase} from "$lib/supabaseClient";
-    import placeholder from "$lib/assets/pp.jpg"
-
     export let data;
     let pageData = data.restaurant[0];
-
-    /*function getRestaurantThumbnail(name) {
-        const {data, error} = supabase.storage.from('images').getPublicUrl(`restaurants-thumbnails/${name}`);
-
-        if (error || !name) return placeholder;
-
-        return data.publicUrl;
-    }*/
-
 </script>
 
 <img src={pageData['thumbnail']} alt={pageData.name}>
