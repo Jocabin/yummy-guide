6e2623d3a852bcd7edeecdf554c46156924ad1b2
Jocabin <kobyfar@hotmail.com>
2022-11-16 15:31:55 +0100

    add restaurant tile for global restos page

---
+++ src/lib/components/RestaurantTile.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let info;
+</script>
+
+<article>
+    <img src={info.thumbnail} alt={`Image de la fiche restaurant "${info.name}"`}>
+
+    <section>
+        <h2>{info.name}</h2>
+        <p>{info.description}</p>
+<!--        todo add tags-->
+    </section>
+</article>
+
+<style>
+    article {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: flex-start;
+        justify-content: flex-start;
+    }
+
+    article > img {
+        background-color: #ddd;
+        width: 100%;
+        aspect-ratio: 16 / 9;
+        border-radius: 1rem;
+    }
+
+    section > h2 {
+        margin: 0;
+    }
+</style>
--- src/routes/restaurants/+page.svelte
+++ src/routes/restaurants/+page.svelte
@@ -1,7 +1,20 @@
 <script>
+    import RestaurantTile from "$lib/components/RestaurantTile.svelte";
+
     export let data;
 </script>
 
-{#each data.data as restaurant (restaurant.id)}
-    <p>{restaurant.name}</p>
-{/each}
+<section class="gridLayout">
+    {#each data.data as restaurant (restaurant.id)}
+        <RestaurantTile info={restaurant}/>
+    {/each}
+</section>
+
+<style>
+    .gridLayout {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, 370px);
+        gap: 4rem;
+        width: 100%;
+    }
+</style>
