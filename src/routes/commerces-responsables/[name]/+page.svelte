9465206e3db1f78edf62551a44644c35da9a73d1
Jocabin <kobyfar@hotmail.com>
2022-11-17 15:30:24 +0100

    fiche resto

--- src/routes/commerces-responsables/[name]/+page.svelte
+++ src/routes/commerces-responsables/[name]/+page.svelte
@@ -1,7 +1,24 @@
 <script>
     //todo map à faire
+    import {supabase} from "$lib/supabaseClient";
+
     export let data;
     let pageData = data.restaurant[0];
+    let tags = [];
+
+    async function getTag(itemId) {
+        const {data, error} = await supabase.from('tags').select().eq("id", itemId);
+        error && console.error(error)
+        return data
+    }
+
+    JSON.parse(pageData.tags).forEach(item => {
+        console.log(item)
+        getTag(item)
+            .then(res => {
+                tags = [...tags, res[0]]
+            })
+    })
 </script>
 
 <section class="heading">
@@ -11,6 +28,11 @@
 </section>
 <section class="infos">
     <div class="wrapper-section">
+        <ul class="tags">
+            {#each tags as tag (tag.id)}
+                <li>{tag.name}</li>
+            {/each}
+        </ul>
         <h1 class="title">{pageData.name}</h1>
         <p>{pageData.description}</p>
     </div>
@@ -21,27 +43,48 @@
 </section>
 
 <style lang="sass">
-    section
-      width: 100%
-      &.heading
-        picture
-          display: block
+  .tags
+    padding: 0
+    margin: 3rem 0
+    display: flex
+    flex-direction: row
+    align-items: center
+    justify-content: flex-start
+    list-style-type: none
+    gap: 1rem
+
+    li
+      border: 2px solid $orange
+      padding: .5rem 1rem
+      border-radius: 100rem
+      color: $orange
+
+  section
+    width: 100%
+
+    &.heading
+      picture
+        display: block
+        width: 100%
+        height: 600px
+
+        img
           width: 100%
-          height: 600px
-          img
-            width: 100%
-            height: 100%
-            object-fit: cover
-            object-position: center center
-      &.infos
-        div.wrapper-section
-          max-width: $maxWidth
-          margin-inline: auto
-          h1.title
-              font-family: 'Forma DJR Micro'
-              font-weight: 500
-              font-size: 24px
-          p.description
-              font-weight: 400
+          height: 100%
+          object-fit: cover
+          object-position: center center
+
+    &.infos
+      div.wrapper-section
+        max-width: $maxWidth
+        margin-inline: auto
+
+        h1.title
+          font-family: 'Forma DJR Micro'
+          font-weight: 500
+          font-size: 24px
+
+        p.description
+          font-weight: 400
 
 </style>
