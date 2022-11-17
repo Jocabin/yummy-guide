01c19805a2827df00b144b2df46ab821a3d06ea2
Florian <f.vander-put@influa.com>
2022-11-17 10:14:09 +0100

    form

--- src/routes/commerces-responsables/ajouter/+page.svelte
+++ src/routes/commerces-responsables/ajouter/+page.svelte
@@ -1,5 +1,23 @@
 <script>
-import Form from "../../../components/Form.svelte";
+    import Form from "../../../components/Form.svelte";
 </script>
-<h1>Ajouter</h1>
-<Form/>
+<section>
+    <div class="wrapper-section">
+        <h1>Ajouter un commerce</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A commodi corporis debitis deserunt enim
+            exercitationem, harum id nihil nobis, odio porro provident quaerat quisquam repellat repudiandae sapiente
+            sit totam vel?</p>
+    </div>
+</section>
+<section class="form">
+    <div class="wrapper-section">
+        <Form/>
+    </div>
+</section>
+
+<style lang="sass">
+  section
+    div.wrapper-section
+      max-width: $maxWidth
+      margin-inline: auto
+</style>
