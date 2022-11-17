dd466caec6acbfd87232e6b65d4885afa6186d1d
Florian <f.vander-put@influa.com>
2022-11-17 11:53:46 +0100

    push

--- src/lib/components/RestaurantTile.svelte
+++ src/lib/components/RestaurantTile.svelte
@@ -1,54 +1,60 @@
 <script>
     export let info;
     import {onMount} from "svelte";
-    import {getLocation} from "$lib/distancematrix.js";
+    import {distanceMatrix, getLocation} from "$lib/distancematrix.js";
+
     let distance;
 
-    onMount(()=>{
-        getLocation(44.85383665167368, -0.5668104057436363).then((data)=>{
-            distance = data;
+    onMount(() => {
+        getLocation().then((data) => {
+            distance = distanceMatrix(data.latitude, data.longitude, info.latitude, info.longitude)
         });
+
+
     })
-    console.log(info)
 </script>
 
 <article>
     <picture>
-        <img src={info.thumbnail} alt={info.name}>
+        <img alt={info.name} src={info.thumbnail}>
     </picture>
     <div class="txt">
+        <p>{distance + " km"}</p>
         <span class="title">{info.name}</span>
         <p class="description">
             {info.description}
         </p>
-        <a href={`/commerces-responsables/`+info.url} class="more">En savoir plus</a>
+        <a class="more" href={`/commerces-responsables/`+info.url}>En savoir plus</a>
     </div>
 </article>
 
 <style lang="sass">
-    article
-      display: flex
-      flex-direction: column
-      gap: 1em
+  article
+    display: flex
+    flex-direction: column
+    gap: 1em
 
-      picture
-        display: block
+    picture
+      display: block
+      width: 100%
+      height: 200px
+      overflow: hidden
+      border-radius: 10px
+
+      img
         width: 100%
-        height: 200px
-        overflow: hidden
-        border-radius: 10px
-        img
-          width: 100%
-          height: 100%
-          object-fit: cover
-          object-position: center center
-      div.txt
-        span.title
-          font-family: 'Forma DJR Micro'
-          font-weight: 500
-          font-size: 24px
-        p.description
-          font-weight: 400
+        height: 100%
+        object-fit: cover
+        object-position: center center
+
+    div.txt
+      span.title
+        font-family: 'Forma DJR Micro'
+        font-weight: 500
+        font-size: 24px
+
+      p.description
+        font-weight: 400
 
 
 </style>
