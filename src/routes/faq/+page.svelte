4de4a0c934077edd24298f0d3d8d7947ddbc338e
Jocabin <kobyfar@hotmail.com>
2022-11-17 09:21:17 +0100

    merge

---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,31 @@
+<h1>Foires aux questions</h1>
+
+<p>
+    Cette FAQ répond aux questions fréquentes, pour toutes autres questions notre formulaire de contact est disponible
+    en bas de page.
+</p>
+
+<section class="questions">
+    <details>
+        <summary>Pourquoi Yummy ?</summary>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut commodi consequuntur corporis cum dignissimos earum hic, iste magni molestias officiis perspiciatis, quidem, quo quos veritatis? Error illum minima nemo!</p>
+    </details>
+</section>
+
+<style>
+
+    .questions {
+        max-width: 950px;
+        width: 100%;
+        padding: 2rem 0;
+    }
+
+    details {
+        cursor: pointer;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-content: flex-start;
+        width: 100%;
+    }
+</style>
