4b92b10b0b916c1c74dad6f744430b87214a0098
Jocabin <kobyfar@hotmail.com>
2022-11-17 11:27:19 +0100

    mentions legales

--- src/routes/mentions-legales/+page.svelte
+++ src/routes/mentions-legales/+page.svelte
@@ -1,22 +1,65 @@
 <script>
     import Breadcrumb from "$lib/components/Breadcrumb.svelte";
 </script>
-<Breadcrumb />
-<section class="heading">
-    <h1>Mentions légales</h1>
-</section>
-<section class="gridLayout">
-    <div class="wrapper-section">
 
+<Breadcrumb />
+
+<h1>Mentions légales</h1>
+
+<section>
+    <div class="wrapper-section">
+        <p style="font-weight: bold">
+            Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site Yummy l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
+        </p>
+
+        <h2>Edition du site</h2>
+
+        <p>
+            Le présent site, accessible à l’URL yummy-guide.netlify.app (le « Site »), est édité par :
+            <br>
+            L’association Yummy, enregistrée auprès de la préfecture/sous-préfecture de 33 - Préfecture Bordeaux sous le numéro XXXXX12345, ayant son siège situé à 5 place Alice Girou 33300 BORDEAUX, représentée par Thélise Tourdiat dûment habilité(e)
+        </p>
+
+        <h2>Hébergement</h2>
+
+        <p>
+            Le Site est hébergé par la société Infomaniak, situé Rue Eugène-Marziano 25, 1227 Genève, Suisse, (contact téléphonique ou email : (+41) 22 820 35 44).
+        </p>
+
+        <h2>Directeur de publication</h2>
+
+        <p>Le Directeur de la publication du Site est Thélise Tourdiat.</p>
+
+        <h2>Nous contacter</h2>
+
+        <p>
+            Par téléphone : +33557142110
+            Par email : thelise.tourdiat@mail-ecv.fr
+            Par courrier : 5 place Alice Girou 33300 BORDEAUX
+        </p>
+
+        <h2>Données personnelles</h2>
+
+        <p>
+            Le traitement de vos données à caractère personnel est régi par notre Charte du respect de la vie privée, disponible depuis la section "Charte de Protection des Données Personnelles", conformément au Règlement Général sur la Protection des Données 2016/679 du 27 avril 2016 («RGPD»).
+        </p>
     </div>
 </section>
 
-<style lang="sass">
-    section
-      div.wrapper-section
-        max-width: $maxWidth
-        margin-inline: auto
-        display: grid
-        grid-template-columns: repeat(3, 1fr)
-        gap: 60px 20px
+<style>
+    section {
+        padding: 2rem;
+    }
+
+    h2, p  {
+        margin: 0;
+    }
+
+    h2 {
+        margin-bottom: 1rem;
+    }
+
+    p {
+        margin-bottom: 3rem;
+    }
 </style>
